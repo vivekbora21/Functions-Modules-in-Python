ff99d3309876fb03b2a37cf5de8aa9656ec0486e
vivekbora21 <108716812+vivekbora21@users.noreply.github.com>
2025-03-21 12:33:10 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+def fact(n):
+    if n < 2:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+num = int(input("Enter a number: "))
+fac = fact(num)
+print(f"Factorial of {num} is: ",fac)
---
+++ Task_2.py
@@ -0,0 +1,5 @@
+import math
+num = int(input("Enter a number: "))
+print("Square root: ",math.sqrt(num))
+print("Logarithm: ",math.log(num))
+print("Sine: ",math.sin(num))
